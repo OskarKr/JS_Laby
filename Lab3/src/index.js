d2c88290e01607ab73449699784b49e202c8c736
OskarKr <oskar.krolik@gmail.com>
2023-01-11 01:00:09 +0100

    Proba recording

--- Lab3/src/index.js
+++ Lab3/src/index.js
@@ -1,3 +1,5 @@
+let recording = false
+let trackNumber = 1
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -12,3 +14,47 @@ function playSound(e) {
   }
 
   window.addEventListener("keydown", playSound);
+
+  let track1 = []
+  let track2 = []
+  let track3 = []
+  let track4 = []
+
+  function record(event) {
+    if (!recording) {
+      return;
+    }
+    const sound = {
+      key: event.key,
+      time: Date.now()
+    };
+    const trackName = eval('track'+trackNumber)
+    trackName.push(sound);
+    console.log(track1);
+  }
+
+  function setRecording(number) {
+    recording = true
+    if (number) {
+      trackNumber = number
+    }
+
+  }
+
+  window.addEventListener('keypress', record)
+
+  function playBack(number){
+    let pauseTime;
+    const trackName = eval('track'+number);
+    trackName.forEach(element => {
+      if (element === trackName[0]) {
+        pauseTime = element.time;
+      }
+      if(element.key) {
+        setTimeout(() => {
+          playSound(element.key)
+        }, element.time - pauseTime)
+      }
+      console.log(pauseTime);
+    });
+  }
